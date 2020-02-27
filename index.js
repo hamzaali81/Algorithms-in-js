d2894672d9f4de46dd0e82c13b0655bf460b3b2a
hamzaali <hamza54834@gmail.com>
2020-02-27 11:24:08 -0800

    practice algorithm

---
+++ index.js
@@ -0,0 +1,43 @@
+
+// // function test(n){
+// //     var total=0;
+// //     for(var i=1;i<=n;i++){
+// //         total=total+i;
+
+
+// //     }
+// //     return total;
+// // }
+// // var a=test(6);
+// // console.log(a);
+
+
+// // function algo(m){
+// //     return m*(m+1)/2
+// // }
+// // console.log(algo(6));
+
+
+
+// // for (t=1;t<=6;t++){
+
+// // }
+// // return t;
+// //  var obj={
+// //      name: 'hamza',
+// //      yearOfbirth: 1999
+// //      }
+
+// // 
+// var a= assert(a===1,"Hello world");
+//      console.log(a);
+// const maxCount=[10,25,58,6]
+// console.time("My operation");
+// for(n=0; n<maxCount;n++){
+
+// }
+//  console.timeEnd("My operation");
+// // console.time("Hello");
+// // console.log("Pakistan");
+// // console.timeEnd("Hello")
+console.log("Hello");
